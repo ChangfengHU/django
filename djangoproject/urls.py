e80c6f64e44d1644fbf52b04204571690588a7c6
ChangfengHU <faker_322@outlook.com>
2019-01-22 03:11:24 +0800

    添加主页

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from search.views import SearchSuggest
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+#     url(r'^$', TemplateView.as_view(template_name="index.html"),name="index"),
+#
+#     url(r'^suggest/$', SearchSuggest.as_view(),name="suggest"),
+#
+# ]
+
+from django.urls import path, re_path
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name="index.html"),name="index"),
+    path('', TemplateView.as_view(template_name="index.html"), name="index"),
+    path('suggest/', SearchSuggest.as_view(), name="suggest"),
 ]
--- search/views.py
+++ search/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from search.models import Article
+from django.http import HttpResponse
+import json
+# Create your views here
+class SearchSuggest(View):
+    def get(self,request):
+        key_words=request.get("s",'')
+        re_date=[]
+        if key_words:
+            search=Article.search()
 
-# Create your views here.
+            source = search.suggest('my_suggest', 'pyton', completion={"field":"suggest",
+                "fuzzy":{
+                    "fuzziness":2
+                },
+                "size":10})
+            suggestions=source.execute()
+            for match in suggestions.my_suggest[0].options:
+                source=match._source
+                re_date.append(source["title"])
+
+            return  HttpResponse(json.dump(re_date),content_type="application/json")
